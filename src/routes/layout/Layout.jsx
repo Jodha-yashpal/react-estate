86ea83d794117c040970ccf637ae328db001e843
Yashpal <yashpalsinghjodha2527@gmail.com>
2024-06-27 17:18:04 +0530

    listpage with filter, card components added

--- src/routes/layout/Layout.jsx
+++ src/routes/layout/Layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import './layout.scss'
 
 function layout() {
   return (
-    <div className='layout'>
          <div className="layout">
             <div className="navbar">
               <Navbar/>
@@ -13,7 +13,6 @@ function layout() {
               <Outlet/>
            </div>
          </div>
-    </div>
   )
 }
 
